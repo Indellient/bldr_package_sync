4d6bad73bf3df5e2006e27153d898456b206ce2c
skylerto <skylerclayne@gmail.com>
2018-12-22 14:40:56 -0500

    fetching package and packages tdeps
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- bldr_api.go
+++ bldr_api.go
@@ -3,8 +3,10 @@ package main
 import (
 	// "github.com/BurntSushi/toml"
 	"encoding/json"
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"os"
 	"time"
@@ -21,9 +23,180 @@ type BldrApi struct {
 	AuthToken string `toml:authToken`
 }
 
+type PackageData struct {
+	Origin  string `json:origin`
+	Name    string `json:name`
+	Version string `json:version`
+	Release string `json:release`
+}
+
+type Packages struct {
+	Start int           `json:"range_start"`
+	End   int           `json:"range_end"`
+	Total int           `json:"total_count"`
+	Data  []PackageData `json:data`
+}
+
+type Package struct {
+	Channels   []string      `json:"channels"`
+	Checksum   string        `json:"checksum"`
+	Config     string        `json:"config"`
+	CreatedAt  string        `json:"created_at"`
+	Deps       []PackageData `json:"deps"`
+	TDeps      []PackageData `json:"tdeps"`
+	Exposes    []int         `json:"exposes"`
+	Id         string        `json:"id"`
+	Ident      PackageData   `json:"ident"`
+	IdentArray []string      `json:"ident_array"`
+	IsAService bool          `json:"is_a_service"`
+	Manifest   string        `json:"manifest"`
+	Name       string        `json:"name"`
+	Origin     string        `json:"origin"`
+	OwnerId    string        `json:"owner_id"`
+	Target     string        `json:"target"`
+	UpdatedAt  string        `json:"updated_at"`
+	Visibility string        `json:"visibility"`
+}
+
+// Package dependencies are allows in the stable channel
+// Therefore we should never include the package we're dealing with
+// in its tdeps array
+func (api BldrApi) fetchPackageDeps(pkg PackageData) []PackageData {
+	var pkgs []PackageData
+
+	data := api.fetchPackage(pkg)
+
+	if len(data.TDeps) <= 0 {
+		return pkgs
+	}
+
+	for _, p := range data.TDeps {
+		if len(pkgs) <= 0 {
+			pkgs = []PackageData{p}
+		}
+		return append(pkgs, api.fetchPackageDeps(p)...)
+	}
+
+	return pkgs
+}
+
+func (api BldrApi) fetchPackage(pkg PackageData) Package {
+	var data Package
+	pkgName := fmt.Sprintf("%s/%s/%s/%s", pkg.Origin, pkg.Name, pkg.Version, pkg.Release)
+
+	url := fmt.Sprintf("%s/v1/depot/pkgs/%s", api.Url, pkgName)
+
+	log.Debug("HTTP GET " + url)
+
+	client := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, getErr := client.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	jsonErr := json.Unmarshal(body, &data)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return data
+}
+
+func (api BldrApi) listAllPackages(origin string, channel string) Packages {
+	packages := api.listPackages(origin, channel)
+	resultsPerPage := float64(packages.End - packages.Start)
+	iterations := math.Ceil(float64(packages.Total) / resultsPerPage)
+	for i := float64(1); i <= iterations; i++ {
+		pkgs := api.listPackagesRange(origin, channel, int(i*resultsPerPage))
+		packages.Data = append(packages.Data, pkgs.Data...)
+	}
+	return packages
+}
+
+func (api BldrApi) listPackages(origin string, channel string) Packages {
+	PACKGE_PATH := "/v1/depot/channels/" + origin + "/" + channel + "/pkgs"
+
+	url := api.Url + PACKGE_PATH
+	log.Debug("HTTP GET " + url)
+
+	client := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, getErr := client.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	var pkgs Packages
+	jsonErr := json.Unmarshal(body, &pkgs)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return pkgs
+}
+
+func (api BldrApi) listPackagesRange(origin string, channel string, count int) Packages {
+	// PACKGE_PATH := "/v1/depot/channels/" + origin + "/" + channel + "/pkgs?range=" + count
+	PACKGE_PATH := fmt.Sprintf("/v1/depot/channels/%s/%s/pkgs?range=%d", origin, channel, count)
+
+	url := api.Url + PACKGE_PATH
+	log.Debug("HTTP GET " + url)
+
+	client := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, getErr := client.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	var pkgs Packages
+	jsonErr := json.Unmarshal(body, &pkgs)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return pkgs
+}
+
 func (api BldrApi) fetchKeyPaths(origin string) []OriginKey {
 	KEY_PATH := "/v1/depot/origins/" + origin + "/keys"
-	log.Debug("Fetching all key paths for " + origin + " against bldr " + api.Url)
 
 	url := api.Url + KEY_PATH
 	log.Debug("HTTP GET " + url)
@@ -52,14 +225,12 @@ func (api BldrApi) fetchKeyPaths(origin string) []OriginKey {
 	if jsonErr != nil {
 		log.Fatal(jsonErr)
 	}
-	log.Debug(keys)
 
 	return keys
 }
 
 func (api BldrApi) fetchKeyData(key OriginKey) string {
 	KEY_PATH := "/v1/depot" + key.Location
-	log.Debug("Fetching key data for " + key.Origin + " rev " + key.Revision)
 
 	url := api.Url + KEY_PATH
 	log.Debug("HTTP GET " + url)
--- bldr_api_test.go
+++ bldr_api_test.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
+
+// func TestListPackages(t *testing.T) {
+// 	api := BldrApi{Url: "https://bldr.habitat.sh"}
+// 	pkgs := api.listAllPackages("core", "stable")
+
+// 	if len(pkgs.Data) <= 0 {
+// 		t.Error("Fetching Packages returned an slice <= 0")
+// 	}
+
+// 	if len(pkgs.Data) < 4737 {
+// 		t.Error("Fetching All Packages failed, highly doubt packages from core were deleted")
+// 	}
+// }
+
+func TestFetchPackageDeps(t *testing.T) {
+	api := BldrApi{Url: "https://bldr.habitat.sh"}
+	data := PackageData{Origin: "core", Name: "airlock", Version: "10", Release: "20171027222310"}
+	pkg := api.fetchPackageDeps(data)
+	fmt.Println(pkg)
+
+	if len(pkg) <= 0 {
+		t.Error("Error fetching package info")
+	}
+
+}
+
+func TestFetchPackageData(t *testing.T) {
+	api := BldrApi{Url: "https://bldr.habitat.sh"}
+	data := PackageData{Origin: "core", Name: "airlock", Version: "10", Release: "20171027222310"}
+	pkg := api.fetchPackage(data)
+
+	if pkg.Name != "airlock" {
+		t.Error("Error fetching package info")
+	}
+
+}
 
 func TestFetchKeyPaths(t *testing.T) {
 	api := BldrApi{Url: "https://bldr.habitat.sh"}
