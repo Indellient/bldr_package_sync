aa5dfbafb2c0c0e252d3144e264c02aa8b03897b
skylerto <skylerclayne@gmail.com>
2018-12-23 11:54:24 -0500

    modify log level
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- syncer.go
+++ syncer.go
@@ -37,34 +37,32 @@ func (syncer Syncer) syncPackages(origin string, channel string, upstream BldrAp
 		// go func(wg *sync.WaitGroup) {
 		files := []string{}
 		deps := upstream.fetchPackageDeps(p)
-		log.Debug(fmt.Sprintf("Determined deps %s", deps))
+		log.Info(fmt.Sprintf("Determined deps %s", deps))
 		for i, pkg := range deps {
 			pkgName := fmt.Sprintf("%s/%s/%s/%s", pkg.Origin, pkg.Name, pkg.Version, pkg.Release)
 
-			log.Debug(fmt.Sprintf("Dependancy [%d/%d] %s", i+1, len(deps), pkgName))
+			log.Info(fmt.Sprintf("Dependancy [%d/%d] %s", i+1, len(deps), pkgName))
 			if !target.packageExists(pkg) {
 				pack := upstream.fetchPackage(pkg)
-				// if targPack.Name != "" {
-				log.Debug(fmt.Sprintf("Downloading package %s for target %s", pack.Name, pack.Target))
+				log.Info(fmt.Sprintf("Downloading package %s for target %s", pack.Name, pack.Target))
 				file := upstream.downloadPackage(pack)
-				// log.Debug("Uploading package " + pkgName)
-				// packageUpload(target, file, "stable")
 				files = append(files, file)
 			} else {
-				log.Debug(fmt.Sprintf("Dependancy %s exists in target, skipping download", pkgName))
+				log.Info(fmt.Sprintf("Dependancy %s exists in target, skipping download", pkgName))
 			}
 		}
 
-		log.Debug(fmt.Sprintf("package [%d/%d]", j, len(pkgDatas)))
+		log.Info(fmt.Sprintf("package [%d/%d]", j, len(pkgDatas)))
 		pack := upstream.fetchPackage(p)
 		pkgName := fmt.Sprintf("%s/%s/%s/%s", p.Origin, p.Name, p.Version, p.Release)
 		log.Debug(fmt.Sprintf("Downloading package %s for target %s", pack.Name, pack.Target))
 		file := upstream.downloadPackage(pack)
-		log.Debug("Uploading package " + pkgName)
+		log.Info("Uploading package " + pkgName)
 		packageUpload(target, file, "stable")
 		files = append(files, file)
 
 		for _, file := range files {
+			log.Info("Cleaning up downloaded files")
 			log.Debug("Removing file ", file)
 			os.Remove(file)
 		}
